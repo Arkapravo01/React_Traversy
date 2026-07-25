a23edb324f8f233ec73411d1ef4866caced76370
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-07-25 17:04:34 +0530

    19

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const user = {
-  name: 'Brad',
-  address: { city: '' },
+// fetch('https://jsonplaceholder.typicode.com/posts/1')
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+const fetchData = async () => {
+  try {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/posts/1',
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-console.log(user.address?.city || 'Unknown');
+fetchData();
