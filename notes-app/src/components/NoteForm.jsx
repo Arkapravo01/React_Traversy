aa31323c7cf51eace734fe0c7e5b5f56aefe0eb5
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-07-26 17:58:50 +0530

    45

--- notes-app/src/components/NoteForm.jsx
+++ notes-app/src/components/NoteForm.jsx
@@ -1,72 +1,121 @@
 import { useState } from 'react';
 
-const NoteForm = () => {
-  const [title, setTitle] = useState('');
-  const [priority, setPriority] = useState('Medium');
-  const [category, setCategory] = useState('Work');
-  const [description, setDescription] = useState('');
+const NoteForm = ({ notes, setNotes }) => {
+  const [formData, setFormData] = useState({
+    title: '',
+    category: 'Work',
+    priority: 'Medium',
+    description: '',
+  });
+
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validation
+    if (!formData.title || !formData.description) return;
+
+    //Create note object
+    const newNote = { id: Date.now(), ...formData };
+
+    // Add notes to state
+    setNotes([newNote, ...notes]);
+
+    //Reset form data
+    setFormData({
+      title: '',
+      category: 'Work',
+      priority: 'Medium',
+      description: '',
+    });
+  };
 
   return (
-    <form className='mb-6'>
-      <div className='mb-4'>
-        <label htmlFor='title' className='block font-semibold'>
-          Title
-        </label>
-        <input
-          type='text'
-          className='w-full p-2 border rounded-lg'
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-      </div>
-      <div className='mb-4'>
-        <label htmlFor='category' className='block font-semibold'>
-          Category
-        </label>
-        <select
-          type='text'
-          className='w-full p-2 border rounded-lg'
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value='Work'>💼 Work</option>
-          <option value='Personal'>🏠 Personal</option>
-          <option value='Ideas'>💡 Ideas</option>
-        </select>
-      </div>
-      <div className='mb-4'>
-        <label htmlFor='priority' className='block font-semibold'>
-          Priority
-        </label>
-        <select
-          type='text'
-          className='w-full p-2 border rounded-lg'
-          value={priority}
-          onChange={(e) => setPriority(e.target.value)}
-        >
-          <option value='High'>🔴 High</option>
-          <option value='Medium'>🟡 Medium</option>
-          <option value='Low'>🟢 Low</option>
-        </select>
-      </div>
-      <div className='mb-4'>
-        <label htmlFor='description' className='block font-semibold'>
-          Description
-        </label>
-        <textarea
-          type='text'
-          className='w-full p-2 border rounded-lg'
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        ></textarea>
-      </div>
+    <>
+      {/*Toggle Button*/}
       <button
-        className='w-full bg-purple-500 text-white py-2 
-      rounded-lg cursor-pointer hover:bg-purple-600'
+        onClick={() => setIsFormVisible(!isFormVisible)}
+        className='w-full bg-gray-300 border-gray-300 text-purple-800 py-2 rounded-lg cursor-pointer hover:bg-purple-300 hover: border-purple-400 transition mb-4'
       >
-        Add Note
+        {isFormVisible ? 'Hide Form ❌' : 'Add New Note ➕'}
       </button>
-    </form>
+
+      {/*Form*/}
+      {isFormVisible && (
+        <form onSubmit={handleSubmit} className='mb-6'>
+          <div className='mb-4'>
+            <label htmlFor='title' className='block font-semibold'>
+              Title
+            </label>
+            <input
+              name='title'
+              type='text'
+              className='w-full p-2 border rounded-lg'
+              value={formData.title}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='mb-4'>
+            <label htmlFor='category' className='block font-semibold'>
+              Category
+            </label>
+            <select
+              name='category'
+              type='text'
+              className='w-full p-2 border rounded-lg'
+              value={formData.category}
+              onChange={handleChange}
+            >
+              <option value='Work'>💼 Work</option>
+              <option value='Personal'>🏠 Personal</option>
+              <option value='Ideas'>💡 Ideas</option>
+            </select>
+          </div>
+          <div className='mb-4'>
+            <label htmlFor='priority' className='block font-semibold'>
+              Priority
+            </label>
+            <select
+              name='priority'
+              type='text'
+              className='w-full p-2 border rounded-lg'
+              value={formData.priority}
+              onChange={handleChange}
+            >
+              <option value='High'>🔴 High</option>
+              <option value='Medium'>🟡 Medium</option>
+              <option value='Low'>🟢 Low</option>
+            </select>
+          </div>
+          <div className='mb-4'>
+            <label htmlFor='description' className='block font-semibold'>
+              Description
+            </label>
+            <textarea
+              name='description'
+              type='text'
+              className='w-full p-2 border rounded-lg'
+              value={formData.description}
+              onChange={handleChange}
+            ></textarea>
+          </div>
+          <button
+            className='w-full bg-purple-500 text-white py-2 
+      rounded-lg cursor-pointer hover:bg-purple-600'
+          >
+            Add Note
+          </button>
+        </form>
+      )}
+    </>
   );
 };
 
